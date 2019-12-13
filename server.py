e66365243a60985ce269e83ba867b4eacf69091b
Andrew Lunde <andrew.lunde@sap.com>
2019-12-13 17:27:13 -0500

    R 1

--- server.py
+++ server.py
@@ -135,7 +135,7 @@ def unauth_test():
     if stderr:
         output += stderr + "\n"
 
-    MyOut = subprocess.Popen(['cf', 'login', '-u', 'andrew.lunde@sap.com', '-p', 'Rezin888!', '-o', 'ConcileTime', '-s', 'dev'],
+    MyOut = subprocess.Popen(['cf', 'login', '-u', 'andrew.lunde@sap.com', '-p', 'R!', '-o', 'ConcileTime', '-s', 'dev'],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
     stdout,stderr = MyOut.communicate()
